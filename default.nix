d2438f6f6a402f2516621663c1c4a16e8c0723c8
Tim Lester <tfrancislester@gmail.com>
2026-05-17 15:56:50 -0400

    feat: deploy in GHA with nix

---
+++ default.nix
@@ -0,0 +1,26 @@
+{...}:
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { };
+in
+rec {
+  shells = {
+    default = pkgs.mkShell {
+      packages =  [ pkgs.npins ];
+      buildInputs = [ pkgs.hugo ];
+    };
+  };
+  package = {
+    default = pkgs.stdenv.mkDerivation {
+      name = "tfrancisl.github.io";
+      src = ./.;
+
+      buildPhase = ''
+        ${pkgs.hugo}/bin/hugo build --gc --minify
+      '';
+
+      installPhase = "cp -r public $out";
+    };
+  };
+  default = shells.default;
+}
