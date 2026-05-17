bedda12476ba74beda2fa6adf3840b26578eb5de
Tim Lester <tfrancislester@gmail.com>
2026-05-17 18:16:06 -0400

    fix: add FOD for Hugo modules

--- default.nix
+++ default.nix
@@ -2,21 +2,39 @@
 let
   sources = import ./npins;
   pkgs = import sources.nixpkgs { };
+  nativeBuildInputs = [ pkgs.hugo pkgs.go pkgs.git ];
 in
 rec {
   shells = {
     default = pkgs.mkShell {
       packages =  [ pkgs.npins ];
-      buildInputs = [ pkgs.hugo pkgs.go pkgs.git ];
+      inherit nativeBuildInputs;
     };
   };
   package = {
-    default = pkgs.stdenv.mkDerivation {
+
+    default = let
+    hugoModules = pkgs.stdenvNoCC.mkDerivation {
+        name = "hugo modules";
+        outputHashMode = "recursive";
+        inherit nativeBuildInputs;
+        src = ./.;
+        outputHash = "sha256-kuyhB/MTpcKc/HHWlHW2XtQAElpFF6cn+HadniLNC3g=";
+        buildPhase = "hugo mod vendor";
+        installPhase = ''
+        cp -r _vendor/ $out
+        '';
+    };
+    in
+    pkgs.stdenvNoCC.mkDerivation {
       name = "tfrancisl.github.io";
       src = ./.;
+      inherit nativeBuildInputs;
 
       buildPhase = ''
-        ${pkgs.hugo}/bin/hugo build --gc --minify
+        mkdir -p _vendor/
+        cp -r ${hugoModules}/* _vendor/
+        hugo build --gc --minify
       '';
 
       installPhase = "cp -r public $out";
