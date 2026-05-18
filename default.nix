4edaabe7b9dedec3b45e408a1977204d645b7b1e
Tim Lester <tfrancislester@gmail.com>
2026-05-18 18:56:23 -0400

    feat: drop Ananke altogether

--- default.nix
+++ default.nix
@@ -2,41 +2,21 @@
 let
   sources = import ./npins;
   pkgs = import sources.nixpkgs { };
-  nativeBuildInputs = [ pkgs.hugo pkgs.go pkgs.git ];
+  nativeBuildInputs = [ pkgs.hugo pkgs.git ];
 in
 rec {
   shells = {
     default = pkgs.mkShell {
-      packages =  [ pkgs.npins ];
+      packages = [ pkgs.npins ];
       inherit nativeBuildInputs;
     };
   };
   package = {
-
-    default = let
-    hugoModules = pkgs.stdenvNoCC.mkDerivation {
-        name = "hugo modules";
-        outputHashMode = "recursive";
-        inherit nativeBuildInputs;
-        src = ./.;
-        outputHash = "sha256-kuyhB/MTpcKc/HHWlHW2XtQAElpFF6cn+HadniLNC3g=";
-        buildPhase = "hugo mod vendor";
-        installPhase = ''
-        cp -r _vendor/ $out
-        '';
-    };
-    in
-    pkgs.stdenvNoCC.mkDerivation {
+    default = pkgs.stdenvNoCC.mkDerivation {
       name = "tfrancisl.github.io";
       src = ./.;
       inherit nativeBuildInputs;
-
-      buildPhase = ''
-        mkdir -p _vendor/
-        cp -r ${hugoModules}/* _vendor/
-        hugo build --gc --minify
-      '';
-
+      buildPhase = "hugo build --gc --minify";
       installPhase = "cp -r public $out";
     };
   };
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/tfrancisl/tfrancisl.github.io
-
-go 1.26.2
-
-require github.com/gohugo-ananke/ananke/v2 v2.17.0 // indirect
