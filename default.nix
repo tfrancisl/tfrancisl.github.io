de4bbf5917e794ffd32fb5d361c419d4810f2db3
Tim Lester <tfrancislester@gmail.com>
2026-05-18 18:57:21 -0400

    feat: base html layouts, simple.css

--- default.nix
+++ default.nix
@@ -14,7 +14,15 @@ rec {
   package = {
     default = pkgs.stdenvNoCC.mkDerivation {
       name = "tfrancisl.github.io";
-      src = ./.;
+      # Include .git/ so Hugo can populate per-page .GitInfo in the sandbox.
+      # Nix copies this to the build dir (owned by the build user), so git works.
+      src = builtins.path {
+        path = ./.;
+        name = "tfrancisl.github.io-src";
+        filter = path: type:
+          let base = baseNameOf path; in
+          base != "public" && base != "result" && base != ".direnv";
+      };
       inherit nativeBuildInputs;
       buildPhase = "hugo build --gc --minify";
       installPhase = "cp -r public $out";
